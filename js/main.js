7a7368f95e76f64a083caf3d39da51a8bb68e1dd
stonka123 <stonka123@gmail.com>
2022-03-23 12:22:11 +0100

    header aboutme

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const burgerBtn = document.querySelector('.hamburger')
 const nav = document.querySelector('.mobile')
 const navItems = document.querySelectorAll('.mobile__item')
+const main = document.querySelector('.main')
+const burgerBars = document.querySelector('.hamburger-inner')
 
 const showMenu = () => {
 	burgerBtn.classList.toggle('is-active')
@@ -10,8 +12,19 @@ const showMenu = () => {
 		item.addEventListener('click', () => {
 			nav.classList.remove('mobile__show')
 			burgerBtn.classList.remove('is-active')
+			burgerBars.classList.remove('hamburger-rainbow')
 		})
 	})
 }
 
+const handleObserve = () => {
+	const currentY = window.scrollY
+	if (main.classList.contains('main') && main.offsetTop <= currentY + 30) {
+		burgerBars.classList.add('hamburger-rainbow')
+	} else {
+		burgerBars.classList.remove('hamburger-rainbow')
+	}
+}
+
 burgerBtn.addEventListener('click', showMenu)
+window.addEventListener('scroll', handleObserve)
