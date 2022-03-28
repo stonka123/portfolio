feb67fa1f842b6e7f205b2bcf807a11b8af2b744
stonka123 <stonka123@gmail.com>
2022-03-28 20:08:12 +0200

    validate form

--- js/main.js
+++ js/main.js
@@ -4,7 +4,51 @@ const navItems = document.querySelectorAll('.mobile__item')
 const main = document.querySelector('.main')
 const burgerBars = document.querySelector('.hamburger-inner')
 const footerYear = document.querySelector('.footer--year')
+// FORM VARIABLES
+const username = document.querySelector('#username')
+const email = document.querySelector('#email')
+const subject = document.querySelector('#subject')
+const msg = document.querySelector('#msg')
+const sendBtn = document.querySelector('.form-button')
 
+// FORM VALIDATION
+const formInputs = [username, email, subject, msg]
+
+const showError = (input, errorView) => {
+	const formBox = input.parentElement
+	const errorMsg = formBox.querySelector('.error')
+	formBox.classList.add('error-show')
+	formBox.classList.add('error-border')
+
+	errorMsg.textContent = errorView
+}
+const clearError = input => {
+	const formBox = input.parentElement
+	formBox.classList.remove('error-show')
+	formBox.classList.remove('error-border')
+}
+
+const checkForm = input => {
+	formInputs.forEach(el => {
+		if (el.value === '') {
+			showError(el, el.placeholder)
+		} else {
+			clearError(el)
+		}
+	})
+}
+
+const checkMail = email => {
+	const re = /^[a-zA-Z0-9]+([-._][a-zA-Z0-9]+)*@[a-zA-Z0-9]+([-.][a-zA-Z0-9]+)*\.[a-zA-Z]{2,7}$/
+
+	if (re.test(email.value)) {
+		clearError(email)
+	} else {
+		showError(email, 'E-mail jest niepoprawny')
+	}
+}
+
+// HAMBTN
 const showMenu = () => {
 	burgerBtn.classList.toggle('is-active')
 	nav.classList.toggle('mobile__show')
@@ -17,12 +61,12 @@ const showMenu = () => {
 		})
 	})
 }
-
+// FOOTER
 const observeYear = () => {
 	const year = new Date().getFullYear()
 	footerYear.textContent = year
 }
-
+// OBSERVE HAMBTN COLOR SECTION
 const handleObserve = () => {
 	const currentY = window.scrollY
 	if (main.classList.contains('main') && main.offsetTop <= currentY + 30) {
@@ -31,6 +75,12 @@ const handleObserve = () => {
 		burgerBars.classList.remove('hamburger-rainbow')
 	}
 }
+
 observeYear()
 burgerBtn.addEventListener('click', showMenu)
 window.addEventListener('scroll', handleObserve)
+sendBtn.addEventListener('click', e => {
+	e.preventDefault()
+	checkForm(formInputs)
+	checkMail(email)
+})
